302cde720f8aac0e22f6d7a0798ff68ed7ae3afe
HibaBader <studenthibabader@gmail.com>
2021-03-06 20:04:12 +0200

    subjectsList-version1

---
+++ app/src/main/java/com/example/cscommunity/Adapter.java
@@ -0,0 +1,60 @@
+package com.example.cscommunity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    LayoutInflater inflater;
+    List<Subject> subjects;
+
+    public Adapter(Context context, List<Subject> subjects){
+        this.inflater=LayoutInflater.from(context);
+        this.subjects = subjects;
+
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view =inflater.inflate(R.layout.custom_list_layout,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.subjectName.setText(subjects.get(position).getName());
+        Picasso.get().load(subjects.get(position).getImageURL()).into(holder.subjectImage);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return subjects.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView subjectName;
+        ImageView subjectImage;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            subjectName=itemView.findViewById(R.id.Subject);
+            subjectImage=itemView.findViewById(R.id.coverImage);
+
+        }
+    }
+}
